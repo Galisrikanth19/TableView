e896d66ed4bfab5088370069d9fe04ced06141d1
Gali Srikanth <GaliSrikanth@gali-srikanth-mba.local>
2018-09-20 08:41:50 +0530

    Few issues fixed

---
+++ TableView/MVC/Model/ModelClass.swift
@@ -0,0 +1,12 @@
+//
+//  ModelClass.swift
+//  TableView
+//
+//  Created by Gali Srikanth on 01/08/18.
+//  Copyright © 2018 Gs19. All rights reserved.
+//
+
+import UIKit
+
+class ModelClass: NSObject {
+}
